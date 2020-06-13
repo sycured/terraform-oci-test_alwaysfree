ceddf3fe1528f29124154b508cc2f9ddd5c5229c
sycured <60801403+sycured@users.noreply.github.com>
2020-06-12 22:03:23 -0500

    switch http/https to stateless [skip ci]

--- security.tf
+++ security.tf
@@ -19,11 +19,12 @@ resource "oci_core_security_list" "security_list_ssh" {
     source   = "0.0.0.0/0"
 
     tcp_options {
-      max = "22"
       min = "22"
+      max = "22"
     }
   }
 }
+
 resource "oci_core_security_list" "security_list_web" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.vcn.id
@@ -32,20 +33,22 @@ resource "oci_core_security_list" "security_list_web" {
   ingress_security_rules {
     protocol = "6"
     source   = "0.0.0.0/0"
+    stateless = true
 
     tcp_options {
-      max = "80"
       min = "80"
+      max = "80"
     }
   }
 
   ingress_security_rules {
     protocol = "6"
     source   = "0.0.0.0/0"
+    stateless = true
 
     tcp_options {
-      max = "443"
       min = "443"
+      max = "443"
     }
   }
 }
